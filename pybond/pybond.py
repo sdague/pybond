165a18a05d202fc0596cdc2cfff0006574d86bd9
Sean Dague <sean@dague.net>
2019-05-27 06:42:42 -0400

    add initial pybond class

--- pybond/pybond.py
+++ pybond/pybond.py
@@ -1,3 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import requests
+
 """Main module."""
+
+class Hub(object):
+
+    def __init__(self, addr, token):
+        self._addr = addr
+        self._token = token
+        self.client = requests.Session()
+        self.client.headers.update({"BOND-Token": self._token})
+
+    def _url(self, url):
+        return "http://{}/v2{}".format(self._addr, url)
+
+    def _get(self, path):
+        return self.client.get(self._url(path))
+
+    @property
+    def devices(self):
+        return self._get("/devices")
